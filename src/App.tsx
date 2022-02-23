170ebcb381452772e1fe664bcfdabe95b5d1dbee
kamoto <kamototakahiro@kamototakahironoMacBook-Air.local>
2022-02-23 23:34:59 +0900

    handleOnSubmitの追加

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,28 @@ type Todo = {
 function App() {
   const [text, setText] = useState('');
 
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleOnSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+    };
+
+    setTodos([newTodo, ...todos]);
+
+    setText('');
+  };
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        handleOnSubmit();
+      }}>
         <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-        <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
+        <input type="submit" value="追加" onSubmit={handleOnSubmit} />
       </form>
     </div>
   );
