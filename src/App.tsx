4320d60469f87fba3bd086fbc82b2c1c200cca08
kamoto <kamototakahiro@kamototakahironoMacBook-Air.local>
2022-02-23 21:07:20 +0900

    useStateの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
         <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
       </form>
     </div>
